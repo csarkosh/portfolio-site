580059af2b8373590c46ec727aa7fc8777e91585
Cyrus <csarkosh@uw.edu>
2020-08-01 20:43:39 -0700

    Remove un-needed lambda

--- lambdas/rss-scraper/exports.js
+++
@@ -1,6 +0,0 @@
-const fetch = require('node-fetch');
-const parser = require('rss-parser');
-
-exports.handler = async () => {
-    // TODO Fetch, transform, and cache my Medium articles using RSS
-};

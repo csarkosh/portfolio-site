26461cc9d41d2c4b42cb7a529885ceaa3e31055b
Cyrus <csarkosh@uw.edu>
2020-08-15 12:23:02 -0700

    Update wording

--- src/Contact.js
+++ src/Contact.js
@@ -11,7 +11,7 @@ export default function() {
             <p>
                 Looking to get in touch? I'd love to hear from you. The best way to
                 get in touch with me is to send me a digital note at
-                &nbsp;<a aria-label="Email" href="mailto:cyrussarkosh@gmail.com">cyrussarkosh@gmail.com</a>
+                &nbsp;<a aria-label="Email" href="mailto:mail.4.csarkosh@gmail.com">mail.4.csarkosh@gmail.com</a>
             </p>
             <SocialLinks />
         </div>
